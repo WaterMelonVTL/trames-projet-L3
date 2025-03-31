d9229d0e857e0a45bf643858b5a06b29463fd01b
Youssef Ghobara <ghobara.youssef@gmail.com>
2025-03-31 21:18:05 +0200

    Reworked routing & added mexican theme v1.0

--- Front-End-Trame/src/App.tsx
+++ Front-End-Trame/src/App.tsx
@@ -4,9 +4,14 @@ import React from 'react';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import CalendarPage from './pages/CalendarPage';
-//import Snowfall from 'react-snowfall'
 import SetupTrame from './pages/SetupTrame';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeContext, ThemeProvider } from './components/ThemeContext';
+import Snowfall from 'react-snowfall';
+import Confetti from 'react-confetti';
+
+
+
 
 // Create a client
 const queryClient = new QueryClient({
@@ -18,30 +23,75 @@ const queryClient = new QueryClient({
   },
 });
 function App() {
-
-
   return (
-    <div className='w-screen h-screen absolute top-0 left-0'>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path='/calendar/:id' element={<CalendarPage />} />
-            <Route path="/edit">
-              <Route path="trame/:id" element={<SetupTrame />} />
-            </Route>
-          </Routes>
-        </Router>
-      </QueryClientProvider>
-    </div>
-  )
+    <ThemeProvider>
+      <ThemeContext.Consumer>
+        {({ theme }) => (
+          <div className="App">
+            
+            {theme === 'noel' && (
+              <>
+                <div
+                  className="absolute w-full h-16 top-0 z-[999] -translate-y-[17%] left-0"
+                  style={{
+                    backgroundImage: 'url(/assets/noel2.png)',
+                    backgroundRepeat: 'repeat-x',
+                    backgroundSize: 'auto 100%',
+                    backgroundPosition: 'center top',
+                  }}
+                />
+                <Snowfall color="white" style={{ zIndex: 200 }} />
+              </>
+            )}
+            
+            {theme === 'mexicain' && (
+            <>
+              <div
+                className="absolute left-0 w-full h-16 top-0 z-[999]"
+                style={{
+                  backgroundImage: 'url(/assets/mexicain.png)',
+                  backgroundRepeat: 'repeat-x',
+                  backgroundSize: 'auto 150%',
+                  backgroundPosition: 'center top',
+                }}
+              />
+              {/* Sun effect */}
+              <div
+                className="absolute top-12 left-6 w-16 h-16 z-[999] animate-pulse"
+                style={{
+                  backgroundImage: 'url(/assets/sun.svg)',
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                }}
+              />
+              <Confetti
+                width={window.innerWidth}
+                height={window.innerHeight}
+                numberOfPieces={200}
+                recycle={false} // set to false if you want a one-time effect
+              />
+            </>
+          )}
+
+            <div className='w-screen h-screen absolute top-0 left-0'>
+              <QueryClientProvider client={queryClient}>
+                <Router>
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path='/calendar/:id' element={<CalendarPage />} />
+                    <Route path="/edit">
+                      <Route path="trame/:id" element={<SetupTrame />} />
+                    </Route>
+                  </Routes>
+                </Router>
+              </QueryClientProvider>
+            </div>
+          </div>
+        )}
+      </ThemeContext.Consumer>
+    </ThemeProvider>
+  );
 }
 
 export default App
-
-{/* CHRISTMAS THEME
-        <div className="absolute w-full h-16 top-0 z-[999] -translate-y-[17%]" style={{ backgroundImage: 'url(./src/assets/noel2.png)', backgroundRepeat: 'repeat-x', backgroundSize: 'auto 100%' }}>
-      </div>
-      <Snowfall color="white" style={{ zIndex: 200 }} />
-*/}
--- Front-End-Trame/src/pages/Home.tsx
+++ Front-End-Trame/src/pages/Home.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Trame } from '../types/types.ts'
 import { useNavigate } from 'react-router-dom';
+import ThemeSelector from '../components/ThemeSelector';
+import { ThemeContext } from '../components/ThemeContext';
 
 const ARROW = <svg fill="#000000" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xmlSpace="preserve">
   <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
@@ -18,6 +20,21 @@ function Home() {
   const username = "Louis" // Later get it from authentication
 
   const [trames, setTrames] = useState([] as Trame[])
+
+  // theme stuff
+  const { setTheme } = useContext(ThemeContext);
+  const [selectedTheme, setSelectedTheme] = useState('') // by default
+  const themes = [
+    { id: 'noel', name: 'Noel' },
+    { id: 'mexicain', name: 'Mexicain' },
+  ];
+  const handleThemeSelect = (themeId: string) => {
+    setSelectedTheme(themeId);
+    // Propagate the theme to the global context
+    setTheme(themeId);
+    console.log("Selected theme:", themeId);
+  };
+
   const navigate = useNavigate();
 
   const CreateTrame = () => {
@@ -44,31 +61,43 @@ function Home() {
   }, [userID])
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='font-bold text-3xl '>Bienvenue {username}</h1>
+    <div className="relative w-full h-screen">
+      {/* Theme Selection positioned top left */}
+      <div className="absolute top-12 right-6">
+        <h2 className="mb-2 text-xl font-semibold">Thèmes :</h2>
+        <ThemeSelector 
+          themes={themes} 
+          selectedTheme={selectedTheme} 
+          onThemeSelect={handleThemeSelect} 
+        />
+      </div>
 
-      <h2 className='mb-16'>Selectionnez une Trame :</h2>
-      <div className="grid grid-cols-4 gap-2 w-[70vw]">
-        <div className='w-full h-40 border-2 border-black rounded-lg flex items-center p-8 justify-between hover:cursor-pointer hover:bg-blue-500 hover:text-white transition-all duration-300' onClick={CreateTrame}>
-          <h1 className='w-1/2 text-2xl font-bold'>Créer une nouvelle trame</h1>
-          <div className='w-1/4 h-1/2 flex justify-center items-center mr-8'>{ARROW}</div>
-        </div>
-        {trames.length > 0 ? (
-          trames.map((trame, index) => (
-            <div className='w-full h-40 border-2 flex flex-row justify-between items-center p-8 border-black rounded-lg hover:cursor-pointer hover:bg-blue-500 hover:text-white transition-all duration-300' key={index} onClick={() => navigate(`/calendar/${trame.Id}`)}>
-              <div className='flex flex-col justify-around'>
-                <h1>{trame.Name}</h1>
-                <button className='bg-blue-500 text-white rounded-lg px-2' onClick={(e) => {e.stopPropagation(); navigate(`/edit/trame/${trame.Id}`)}}>Editer</button>
-              </div>
-              <div className='w-1/4 h-1/2 flex justify-center items-center'>{ARROW}</div>
-            </div>
-          ))
-        ) : (
-          <div className='w-full h-40 flex flex-col items-center justify-center '>
-            <h1 className='text-2xl font-bold'>Aucune trame trouvée</h1>
-            <h1 className='text-xl font-normal'>Vous pouvez en créer une en utilisant le bouton ci-dessus</h1>
+      <div className='flex flex-col items-center justify-center h-screen'>
+        <h1 className='font-bold text-3xl '>Bienvenue {username}</h1>
+
+        <h2 className='mb-16'>Selectionnez une Trame :</h2>
+        <div className="grid grid-cols-4 gap-2 w-[70vw]">
+          <div className='w-full h-40 border-2 border-black rounded-lg flex items-center p-8 justify-between hover:cursor-pointer hover:bg-blue-500 hover:text-white transition-all duration-300' onClick={CreateTrame}>
+            <h1 className='w-1/2 text-2xl font-bold'>Créer une nouvelle trame</h1>
+            <div className='w-1/4 h-1/2 flex justify-center items-center mr-8'>{ARROW}</div>
           </div>
-        )}
+          {trames.length > 0 ? (
+            trames.map((trame, index) => (
+              <div className='w-full h-40 border-2 flex flex-row justify-between items-center p-8 border-black rounded-lg hover:cursor-pointer hover:bg-blue-500 hover:text-white transition-all duration-300' key={index} onClick={() => navigate(`/calendar/${trame.Id}`)}>
+                <div className='flex flex-col justify-around'>
+                  <h1>{trame.Name}</h1>
+                  <button className='bg-blue-500 text-white rounded-lg px-2' onClick={(e) => {e.stopPropagation(); navigate(`/edit/trame/${trame.Id}`)}}>Editer</button>
+                </div>
+                <div className='w-1/4 h-1/2 flex justify-center items-center'>{ARROW}</div>
+              </div>
+            ))
+          ) : (
+            <div className='w-full h-40 flex flex-col items-center justify-center '>
+              <h1 className='text-2xl font-bold'>Aucune trame trouvée</h1>
+              <h1 className='text-xl font-normal'>Vous pouvez en créer une en utilisant le bouton ci-dessus</h1>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
